ca600abdbc2db31de5205f3813a85a7edab599f9
Damilola Adeniyi <damilolaadeniyi1@gmail.com>
2022-10-10 16:30:38 +0100

    updated files

---
+++ Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "The title must have a minimum length of 3 and a maximum length of 50.")]
+        [Required(ErrorMessage = "Provide Title")]
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Must Include alphabets and space only")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+
+        [Range(1, 1000)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "Must Include alphabhets, numbers and special characters")]
+        [Required]
+        [StringLength(5)]
+        public string? Rating { get; set; }
+    }
+}
